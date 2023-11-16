cacd95a45189e3f967da4db0650d0bb256334bf9
min <min03041999@gmail.com>
2023-11-16 10:46:10 +0700

    feat(travel-app): hanle error

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,8 +16,8 @@ const Footer = () => {
             {FOOTER_LINKS.map((columns) => (
               <FooterColumn title={columns.title}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
-                  {columns.links.map((link) => (
-                    <Link href="/" key={link}>
+                  {columns.links.map((link, index) => (
+                    <Link href="/" key={index}>
                       {link}
                     </Link>
                   ))}
